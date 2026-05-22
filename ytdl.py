a84c38057e7467c7c7558e91fb2f4a9098b8dded
henkka008 <henkka008@hotmail.com>
2026-05-23 00:52:58 +0300

    fixed audio conversion bug where ffmpeg woudlnt work propelly if the audio was opus type, fixed video name error becuase windows dosent like : for some reason so now there is name sanitization.

--- ytdl.py
+++ ytdl.py
@@ -5,6 +5,10 @@ from sys import argv
 import os
 import subprocess
 
+ffmpeg = shutil.which("ffmpeg")
+if ffmpeg is None:
+    raise RuntimeError("ffmpeg not found. Install ffmpeg and add it to PATH")
+
 link = argv[1]
 yt = YouTube(link)
 
@@ -30,7 +34,14 @@ audio = (
 output = './videos'
 os.makedirs(output, exist_ok=True)
 
+def sanitize_filename(filename):
+    invalid_chars = '<>:"/\\|?*'
+    for char in invalid_chars:
+        filename = filename.replace(char, "")
+    return filename.strip()
+
 def get_unique_filename(folder, filename, extension):
+    filename = sanitize_filename(filename)
     counter = 1
     full_path = os.path.join(folder,f"{filename}.{extension}")
     while os.path.exists(full_path):
@@ -39,25 +50,32 @@ def get_unique_filename(folder, filename, extension):
     return full_path
 
 video_path = os.path.join(output, "video.mp4")
-audio_path = os.path.join(output, "audio.mp4")
+audio_ext = audio.subtype
+audio_path = os.path.join(output, f"audio.{audio_ext}")
 final_path = get_unique_filename(output, yt.title, "mp4")
 
 
 
 video.download(output_path=output, filename="video.mp4")
-audio.download(output_path="./videos",filename="audio.mp4")
+audio.download(output_path="./videos",filename=f"audio.{audio_ext}")
 
-ffmpeg = shutil.which("ffmpeg")
-if ffmpeg is None:
-    raise RuntimeError("ffmpeg not found. Install ffmpeg and add it to PATH")
 
-subprocess.run([
+
+ffmpeg_cmd = [
     ffmpeg,
+    "-y",
     "-i", video_path,
     "-i", audio_path,
-    "-c", "copy",
-    final_path
-], check=True)
+    "-c:v", "copy",
+]
+
+if audio_ext == "mp4":
+    ffmpeg_cmd += ["-c:a", "copy"]
+else:
+    ffmpeg_cmd += ["-c:a", "aac", "-b:a", "192k"]
+
+ffmpeg_cmd.append(final_path)
+subprocess.run(ffmpeg_cmd, check=True)
 
 print("Cleaning...")
 os.remove(video_path)
